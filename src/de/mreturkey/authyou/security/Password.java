4e1a11939e31ffd5eaa86f314348815314a86191
mReTurkey <e.isik27@googlemail.com>
2015-12-02 22:46:33 +0200

    Password

--- src/de/mreturkey/authyou/security/Password.java
+++ src/de/mreturkey/authyou/security/Password.java
@@ -1,5 +1,23 @@
 package de.mreturkey.authyou.security;
 
+import de.mreturkey.authyou.util.HashUtils;
+
 public class Password {
 
+	private final String password;
+	private String hash;
+	
+	public Password(String password) {
+		this.password = password;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public String getHash() {
+		if(hash == null) hash = HashUtils.getSha256(password);
+		return hash;
+	}
+	
 }
