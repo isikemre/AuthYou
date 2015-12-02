59125da4ee502831ea8eafbfd7a85915b7402ba8
mReTurkey <e.isik27@googlemail.com>
2015-12-02 16:56:40 +0200

    Init

---
+++ src/de/mreturkey/authyou/AuthYou.java
@@ -0,0 +1,19 @@
+package de.mreturkey.authyou;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AuthYou extends JavaPlugin {
+
+	public void onLoad() {
+		
+	}
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+}
